ff2fd6ab6b612f2ce8c1ea69bf9c1a80058d81d5
runkezhong <jarvisshangye@gmail.com>
2026-05-28 00:52:44 +0800

    refactor(memory): remove legacy embedder module

--- src/mindbot/memory/embedder/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Memory embedder module."""
-
-from mindbot.memory.embedder.base import Embedder
-from mindbot.memory.embedder.openai_embedder import OpenAIEmbedder
-
-__all__ = [
-    "Embedder",
-    "OpenAIEmbedder",
-]
